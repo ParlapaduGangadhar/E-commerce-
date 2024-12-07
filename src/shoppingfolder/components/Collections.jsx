f8b37ac9ab8ccba9312a79ff4df6363731a47d0b
GangadharParlapadu <gangadharaparlapadu59@gmail.com>
2024-12-07 12:16:13 +0530

    first commit

--- src/shoppingfolder/components/Collections.jsx
+++ src/shoppingfolder/components/Collections.jsx
@@ -11,6 +11,7 @@ const {title, image1, image2, image3, image4, image5, image6} = props.gentsFashi
         <img src={image1} alt={title}/>
         <img src={image2} alt={title}/>
         <img src={image3} alt={title}/>
+        
         <img src={image4} alt={title}/>
         <img src={image5} alt={title}/>
         <img src={image6} alt={title}/>

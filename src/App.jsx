ba880031a9976e5bbe92539bc7a25baf66201db0
GangadharParlapadu <gangadharaparlapadu59@gmail.com>
2024-12-07 12:13:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Main from './shoppingfolder/pages/Main'
+
+
+const App = () => {
+  return (
+    <div>
+     <Main/>
+
+
+    </div>
+  )
+}
+
+export default App
